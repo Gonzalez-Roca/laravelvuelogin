5927af863d219335bb1140c2105974c73086f895
eliezer <eligonroc@gmail.com>
2023-03-17 14:48:37 -0600

    3/17/23-14:48

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,19 +4,21 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+            <!-- <div class="card">
+                <div class="card-header">{{ __('Hello word') }}</div> -->
 
-                <div class="card-body">
+                <!-- <div class="card-body">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
-                    @endif
+                    @endif -->
+                    <!-- {{ __('Hola mundo!') }} -->
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+                    <example-component/>
+
+                <!-- </div>
+            </div> -->
         </div>
     </div>
 </div>
